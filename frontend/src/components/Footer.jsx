8dc9fd7c1f67cfc8c24107ef356a21fc82e78708
sanchitpahurkar <pahurkarsm@rknec.edu>
2025-11-23 18:10:08 +0530

    Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-white text-black py-2 flex flex-row justify-center items-center'>
+        <p>Created by <a 
+        href="https://www.linkedin.com/in/sanchit-pahurkar/"
+        className='font-bold text-amber-500'
+        >Sanchit</a>👨‍💻</p>
+    </div>
+  )
+}
+
+export default Footer
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { ToastContainer } from 'react-toastify';
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')).render(
       <BrowserRouter>
         <Navbar />  
         <App />
+        <Footer />
         <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
       </BrowserRouter>
     </AuthProvider>
